65768fdfa8917214e7abca9383047996c462c0a5
Ronen David <ronen@acc.org.il>
2023-10-26 11:29:21 +0300

    improve the code

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-window.onload = function () {
-    alert('What is the secret key binding?');
-};
+// window.onload = function () {
+//     alert('What is the secret key binding?');
+// };
 
 document.addEventListener('keydown', (e) => {
     if (e.key == "b" && e.ctrlKey) {
         alert('you find the secret key binding');
         document.getElementById('success').style.filter = "blur(0px)";
+        document.getElementById('title').style.display = "none";
     }
 });
