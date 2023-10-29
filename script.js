371e775494b9cb316bd91b8fc5b37c690acb2cd5
Ronen David <ronen@acc.org.il>
2023-10-29 21:06:17 +0200

    another selution

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 //     alert('What is the secret key binding?');
 // };
 
-document.addEventListener('keydown', (e) => {
+/* document.addEventListener('keyup', (e) => {
     if (e.key == "b" && e.ctrlKey) {
+        alert('you find the secret key binding');
+        document.getElementById('success').style.filter = "blur(0px)";
+        document.getElementById('title').style.display = "none";
+    }
+}); */
+
+
+document.addEventListener('keyup', function (e) {
+    if ((e.key === "b" || e.key === "B") && e.ctrlKey) {
         alert('you find the secret key binding');
         document.getElementById('success').style.filter = "blur(0px)";
         document.getElementById('title').style.display = "none";
